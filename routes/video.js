242754d21c3d5973316e1a6e48bc6836f3087f68
CYFHero <1753760980@qq.com>
2023-07-12 23:31:05 +0800

    上传接口

---
+++ routes/video.js
@@ -0,0 +1,14 @@
+const router = require("koa-router")();
+const multer = require('@koa/multer');
+router.prefix('/api')
+
+const upload = multer()
+
+router.post('/upload', upload.single('video'), async (ctx) => {
+  console.log('ctx.request.file', ctx.request.file);
+  console.log('ctx.file', ctx.file);
+  console.log('ctx.request.body', ctx.request.body);
+  ctx.body = 'done';
+})
+
+module.exports = router
