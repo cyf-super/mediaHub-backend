3f453ae12952e144047930358968f8ad5d4c127c
CYFHero <1753760980@qq.com>
2023-07-21 23:25:58 +0800

    上传接口

--- controller/upload.js
+++ controller/upload.js
@@ -3,7 +3,7 @@ const fse = require('fs-extra')
 const { ErrorModel, SuccessModel } = require('../model/ResModel')
 const { fileSizeExceedInfo } = require('../model/ErrorInfo')
 
-const DIST_FOLDER_PATH = path.join(__dirname, '..', '..', 'uploadFiles')
+const DIST_FOLDER_PATH = path.join(__dirname, '..', 'uploadFiles')
 fse.pathExists(DIST_FOLDER_PATH).then(exist => {
   if (!exist) {
     fse.ensureDir(DIST_FOLDER_PATH)
@@ -12,12 +12,24 @@ fse.pathExists(DIST_FOLDER_PATH).then(exist => {
 
 const MIX_SIZE = 1024 * 1024 * 204.8
 
-async function saveFile({ name, type, size, filepath }) {
+async function saveFile({ name, category, categoryId, file }) {
+  const { size, mimetype } = file
+  console.log("🚀 ~ saveFile ~ mimetype:", mimetype)
   if (size > MIX_SIZE) {
     return new ErrorModel(fileSizeExceedInfo)
   }
 
+  const filePath = path.join(DIST_FOLDER_PATH, mimetype.split('/')[0])
+  const isExists = await fse.pathExists(filePath)
+  if (!isExists) fse.ensureDir(filePath)
 
+  try {
+    const fileName = Date.now() + '.' + name
+    await fse.writeFile(path.join(filePath, fileName), file.buffer)
+    return '上传成功'
+  } catch (err) {
+    return '上传失败'
+  }
 }
 
 module.exports = {
--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,6 @@
 const router = require("koa-router")();
 const multer = require('@koa/multer');
+const { saveFile } = require('../controller/upload')
 
 router.prefix('/api')
 
@@ -7,17 +8,8 @@ const upload = multer()
 
 router.post('/upload', upload.single('file'), async (ctx) => {
   const file = ctx.file;
-  const category = ctx.request.body.category;
-  const categoryId = ctx.request.body.categoryId;
-  console.log('ctx.file', file);
-  console.log('解码 ', decodeURIComponent(file.originalname));
-  console.log('ctx.request.category', category);
-  console.log('ctx.request.categoryId', categoryId);
-  // const { originalname, mimetype, size } = ctx.file
-  // console.log('originalname--> ', originalname);
-  // console.log('mimetype--> ', mimetype);
-  // console.log('size--> ', size);
-  ctx.body = 'done';
+  const { category, categoryId, name } = ctx.request.body;
+  ctx.body = await saveFile({ category, categoryId, name, file });
 })
 
 
